471ac6651203be27ff93684cf1d30208bd9a5ff4
Vatsal Sanghvi <vatsal1811@gmail.com>
2022-12-08 14:45:44 +0530

    V0 Build

---
+++ pages/api/generate.js
@@ -0,0 +1,25 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = "Write a 10 tweet viral, highly engaging, well articulated, fun twitter thread on the blog post below. Dont use hashtags in any one of the tweets";
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 750,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
